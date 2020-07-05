e981a3854aa7866192aa993989ec50ca3c6102db
vvijayaruban <291619+vvijayaruban@users.noreply.github.com>
2020-07-05 16:50:56 +0100

    simple program

--- day03/01.hs
+++ day03/01.hs
@@ -1,31 +1,42 @@
+-- getIndexWithStart char (x:xs) start =
+--         if x == char 
+--         then start
+--         else
+--             if xs == []
+--             then start
+--             else getIndexWithStart char xs (start + 1)
+
+-- head' array
+--     | [] == array  = ' '
+--     | otherwise = head array
+
+-- getIndexWithStart char az start = case (head' az) of
+--         ' '         -> start
+--         char        -> start
+--         _           -> getIndexWithStart char (tail az) (start + 1)
+
 aToZ = ['a','b' .. 'z'] ++ ['a'] ++ ['A','B' .. 'Z'] ++ ['A']
 
-getIndexWithStart char (x:xs) start =
-        if x == char 
-        then start
-        else
-            if start == 54
-            then (-1)
-            else getIndexWithStart char xs (start + 1)
+getIndexWithStart char (x:xs) start
+       | x == char = start
+       | xs == []  = (-1)
+       | otherwise = getIndexWithStart char xs (start + 1)
 
 getIndex char = getIndexWithStart char aToZ 0
 
 getCharToIndex index = head $ drop index aToZ
 
-encrypt char =
-    getCharToIndex $ (getIndex char) + 1
+encrypt char = getCharToIndex $ (getIndex char) + 1
+
+main = interact (map encrypt)
 
 -- handleChar = do
---     print aToZ
 --     c <- getChar
---     putChar c
 --     let nc = encrypt c
 --     putChar nc
 
 -- inputLoop = do
---     print aToZ
 --     handleChar
+--     inputLoop
 
 -- main = inputLoop
-
-main = interact (map encrypt)
